80bf86c4a12f5a2f1206316dc7f55f9dc3be9fa2
LAPTOP-K715AL44\yonad <yehonadav@feel.tech>
2022-12-03 20:10:21 +0200

    feat(www): added a next app

---
+++ apps/www/pages/_document.tsx
@@ -0,0 +1,50 @@
+/* eslint-disable react/display-name */
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps,
+} from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+export default class CustomDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext,
+  ): Promise<DocumentInitialProps> {
+    const originalRenderPage = ctx.renderPage;
+
+    const sheet = new ServerStyleSheet();
+
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp:
+            App =>
+                props => sheet.collectStyles(
+                  // @ts-ignore
+                  <App
+                    {...props}
+                  />
+                ),
+        enhanceComponent: Component => Component,
+      });
+
+    const intialProps = await Document.getInitialProps(ctx);
+    const styles = sheet.getStyleElement();
+
+    return { ...intialProps, styles };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>{this.props.styles}<title>swarmion-full-stack</title></Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
